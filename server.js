1a8684605253ff427a362993d6a44c982d74a86a
Almanaei <almannaei90@gmail.com>
2026-06-26 01:52:34 +0300

    style: improve light mode color system, accessibility, and contrast

--- server.js
+++ server.js
@@ -658,6 +658,16 @@ erra6BzpXyWJxdylk4cdvD0=
         res.end(content);
       }
     });
+  } else if (req.url === '/dashboardsecondary' || req.url === '/dashboardsecondary.html') {
+    fs.readFile(path.join(__dirname, 'dashboardsecondary.html'), (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading dashboardsecondary.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
   } else if (req.url === '/' || req.url === '/index.html') {
     fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
       if (err) {
